45c1fa02ce752c3140946ebb04693d1ad61b1cf1
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-10-06 18:27:15 +0100

    rendered magazine article data

--- app/magazine/page.tsx
+++ app/magazine/page.tsx
@@ -3,7 +3,7 @@ import PageTitle from "@/components/PageTitle";
 
 export default function MagazinePage() {
   return (
-    <main className="flex min-h-screen flex-col px-4 lg:px-8">
+    <main className="flex min-h-screen flex-col px-4 lg:px-8 max-w-[105rem] mx-auto w-full">
       <PageTitle className="sr-only" imgSrc="/images/titles/Magazine.svg">
         Magazine
       </PageTitle>
--- components/Articles.tsx
+++ components/Articles.tsx
@@ -6,16 +6,26 @@ export default function Articles() {
   const { data } = useArticleContext();
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-      {data.map((article) => (
-        <div key={article.id}>
-          {article.articles.map((articleData, index) => (
-            <div key={index}>
-              <img src={articleData.img} alt={articleData.title} />
+    <div className="max-w-[95rem] w-full mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 border border-black">
+        {data.map((article) =>
+          article.articles.map((articleData, index) => (
+            <div className="border border-black p-8" key={index}>
+              <div className="flex justify-between">
+                <p>{articleData.date}</p>
+                <p>{articleData.label}</p>
+              </div>
+              <img
+                className="w-full"
+                src={articleData.img}
+                alt={articleData.title}
+              />
+              <h2 className="heading3-title">{articleData.title}</h2>
+              <p>{articleData.description}</p>
             </div>
-          ))}
-        </div>
-      ))}
+          ))
+        )}
+      </div>
     </div>
   );
 }
