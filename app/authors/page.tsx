6707d8b7e53db15b1e5aeb143bec922719094f7e
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-10-11 17:37:38 +0100

    rendered author data on page

--- app/authors/page.tsx
+++ app/authors/page.tsx
@@ -1,11 +1,13 @@
+import AuthorsList from "@/components/AuthorList";
 import PageTitle from "@/components/PageTitle";
 
 export default function AuthorsPage() {
   return (
-    <main className="flex min-h-screen flex-col px-4 lg:px-8">
+    <main className="flex flex-col px-4 lg:px-8">
       <PageTitle className="sr-only" imgSrc="/images/titles/Authors.svg">
         Authors
       </PageTitle>
+      <AuthorsList />
     </main>
   );
 }
---
+++ components/AuthorList.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import formatString from "@/app/functions/formatString";
+import { useArticleContext } from "@/hooks/useArticleContext";
+import Link from "next/link";
+
+export default function AuthorsList() {
+  const { data } = useArticleContext();
+
+  return (
+    <div className="flex flex-col gap-12 max-w-[95rem] w-full mx-auto py-8 lg:pt-24 lg:pb-48">
+      {data.map((authors) => (
+        <div
+          className="flex flex-col md:flex-row justify-between md:items-center gap-2 md:gap-0"
+          key={authors.id}
+        >
+          <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-16">
+            <img
+              className="h-[150px] w-[150px]"
+              src={authors.avatar}
+              alt={authors.author}
+            />
+            <h2 className="heading3-title">{authors.author}</h2>
+          </div>
+          <div className="flex flex-col lg:flex-row gap-2 lg:gap-24">
+            <div className="flex gap-2">
+              <p className="font-semibold">Job</p>
+              <p>{authors.job}</p>
+            </div>
+            <div className="flex gap-2">
+              <p className="font-semibold">City</p>
+              <p>{authors.city}</p>
+            </div>
+            <Link
+              className="flex gap-2"
+              href={`podcasts/${formatString(authors.author)}`}
+            >
+              <span className="uppercase font-semibold">About</span>
+              <img
+                src="/icons/ri_arrow-right-line.svg"
+                alt="An arrow pointing right"
+              />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
