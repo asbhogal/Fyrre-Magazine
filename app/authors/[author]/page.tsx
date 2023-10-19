e4bee205275b87be80564d825b82ac257ec1a1e7
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-10-19 14:36:44 +0100

    refactored dynamic rendering of Author details

--- app/authors/[author]/page.tsx
+++ app/authors/[author]/page.tsx
@@ -10,19 +10,6 @@ type AuthorData = {
   };
 };
 
-type AuthorPageProps = {
-  params: {
-    author: string;
-    job: string;
-    city: string;
-    avatar: string;
-    biography: {
-      summary: string;
-      body: string;
-    };
-  };
-};
-
 async function getAuthorDetails() {
   const res = await fetch(
     "https://raw.githubusercontent.com/asbhogal/Fyrre-Magazine/2eac71632cf6b13cfdc8b302ce0589ecf263c03c/json/articles.json"
@@ -32,21 +19,19 @@ async function getAuthorDetails() {
     throw new Error(`HTTP error! Status: ${res.status}`);
   }
 
-  const authors: AuthorData[] = await res.json();
-  return authors;
+  return res.json() as Promise<AuthorData[]>;
 }
 
-export default async function AuthorDetails({ params }: AuthorPageProps) {
+export default async function AuthorDetails({
+  params,
+}: {
+  params: { author: string };
+}) {
   try {
     const authors: AuthorData[] = await getAuthorDetails();
 
-    // Debugging: Log the params.author and normalized slugs
-    console.log("params.author:", params.author);
-    authors.forEach((author) => {
-      console.log("Normalized slug:", author.slug);
-    });
-
     const decodedAuthor = decodeURIComponent(params.author);
+
     const authorData = authors.find(
       (author: AuthorData) => author.slug === decodedAuthor
     );
