01ea74cd5a68b749ee878b3334adfe0a456dabed
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-10-31 17:02:05 +0000

    added missing prop

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,11 @@ export default function NotFound() {
   return (
     <main className="flex flex-col min-h-screen max-w-[95rem] w-full mx-auto px-4 lg:pt-0 md:pt-8 sm:pt-4 xs:pt-2 lg:pb-4 md:pb-4 sm:pb-2 xs:pb-2">
       <div>
-        <PageTitle className="sr-only" imgSrc="/images/titles/NotFound.svg">
+        <PageTitle
+          className="sr-only"
+          imgSrc="/images/titles/NotFound.svg"
+          imgAlt="The words 'Not Found' in bold uppercase lettering"
+        >
           Not Found
         </PageTitle>
         <h2>
