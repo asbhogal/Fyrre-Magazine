0dd673efab30d1ff8eef212c479cf9b36c54584f
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-11-04 01:09:58 +0000

    added SEO data for 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,11 @@
 import PageTitle from "@/components/PageTitle";
 import Link from "next/link";
 
+export const metadata = {
+  title: "Page not found | Fyrre Magazine",
+  description: "Page does not exist",
+};
+
 export default function NotFound() {
   return (
     <main className="flex flex-col min-h-screen max-w-[95rem] w-full mx-auto px-4 lg:pt-0 md:pt-8 sm:pt-4 xs:pt-2 lg:pb-4 md:pb-4 sm:pb-2 xs:pb-2">
