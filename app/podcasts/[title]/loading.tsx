a41a998909e5796f9721e6cf1df11c5bd0488da4
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-11-08 14:11:20 +0000

    further skeleton design for dynamic podcast data

--- app/podcasts/[title]/loading.tsx
+++ app/podcasts/[title]/loading.tsx
@@ -1,3 +1,67 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function Loading() {
-  return <div className="bg-black h-96 w-96">Test</div>;
+  return (
+    <div className="max-w-[95rem] w-full mx-auto px-4 pb-12 md:pt-8 sm:pt-4 xs:pt-2 md:pb-4 sm:pb-2 xs:pb-2">
+      <div className="flex items-center justify-between gap-20 md:gap-0 py-4 md:pt-8 md:pb-24">
+        <div className="flex gap-2">
+          <Skeleton className="bg-[#a1a1a1] h-[1.5625rem] w-[1.5625rem] rounded-none"></Skeleton>
+          <Skeleton className="bg-[#a1a1a1] h-[1.5625rem] w-[5.625rem] rounded-none"></Skeleton>
+        </div>
+        <Skeleton className="bg-[#a1a1a1] h-7 w-[9.875rem] rounded-none"></Skeleton>
+      </div>
+
+      <div className="max-w-[75rem] w-full mx-auto flex flex-col md:flex-row flex-wrap gap-24">
+        <div className="flex flex-col lg:w-1/4">
+          <Skeleton className="bg-[#a1a1a1] h-[300px] w-[300px] rounded-none"></Skeleton>
+          <div className="flex justify-between mt-8 pb-12">
+            <Skeleton className="bg-[#a1a1a1] h-[28px] w-[91px] rounded-none"></Skeleton>
+            <div className="flex gap-3">
+              <Skeleton className="bg-[#a1a1a1] h-6 w-6 rounded-full"></Skeleton>
+              <Skeleton className="bg-[#a1a1a1] h-6 w-6 rounded-full"></Skeleton>
+              <Skeleton className="bg-[#a1a1a1] h-6 w-6 rounded-full"></Skeleton>
+            </div>
+          </div>
+          <Skeleton className="bg-[#a1a1a1] h-0.5 w-full"></Skeleton>
+          <div className="flex flex-col gap-4 pt-8">
+            <div className="flex flex-wrap justify-between">
+              <Skeleton className="bg-[#a1a1a1] h-6 w-10 rounded-none"></Skeleton>
+              <Skeleton className="bg-[#a1a1a1] h-6 w-24 rounded-none"></Skeleton>
+            </div>
+            <div className="flex flex-wrap justify-between">
+              <Skeleton className="bg-[#a1a1a1] h-6 w-10 rounded-none"></Skeleton>
+              <Skeleton className="bg-[#a1a1a1] h-6 w-24 rounded-none"></Skeleton>
+            </div>
+            <div className="flex flex-wrap justify-between">
+              <Skeleton className="bg-[#a1a1a1] h-6 w-10 rounded-none"></Skeleton>
+              <Skeleton className="bg-[#a1a1a1] h-6 w-24 rounded-none"></Skeleton>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col flex-1 w-full">
+          <Skeleton className="bg-[#a1a1a1] h-6 w-28 mb-8 rounded-none"></Skeleton>
+          <div className="flex flex-col gap-2 mb-12">
+            <Skeleton className="bg-[#a1a1a1] h-20 w-full rounded-none"></Skeleton>
+            <Skeleton className="bg-[#a1a1a1] h-20 w-full rounded-none"></Skeleton>
+            <Skeleton className="bg-[#a1a1a1] h-20 w-full rounded-none"></Skeleton>
+            <Skeleton className="bg-[#a1a1a1] h-20 w-full rounded-none"></Skeleton>
+          </div>
+          <div className="flex flex-col gap-2 pt-8 pb-16">
+            <Skeleton className="bg-[#a1a1a1] h-6 w-full rounded-none"></Skeleton>
+            <Skeleton className="bg-[#a1a1a1] h-6 w-full rounded-none"></Skeleton>
+            <Skeleton className="bg-[#a1a1a1] h-6 w-full rounded-none"></Skeleton>
+            <Skeleton className="bg-[#a1a1a1] h-6 w-full rounded-none"></Skeleton>
+            <Skeleton className="bg-[#a1a1a1] h-6 w-full rounded-none"></Skeleton>
+          </div>
+          <div className="flex flex-col gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-full rounded-none"></Skeleton>
+            <Skeleton className="bg-[#a1a1a1] h-4 w-full rounded-none"></Skeleton>
+            <Skeleton className="bg-[#a1a1a1] h-4 w-full rounded-none"></Skeleton>
+            <Skeleton className="bg-[#a1a1a1] h-4 w-full rounded-none"></Skeleton>
+            <Skeleton className="bg-[#a1a1a1] h-4 w-full rounded-none"></Skeleton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/podcasts/[title]/page.tsx
+++ app/podcasts/[title]/page.tsx
@@ -3,7 +3,7 @@ import LatestPodcasts from "@/components/LatestPodcasts/LatestPodcasts";
 import PostNavigation from "@/components/PostNavigation";
 import SocialSharing from "@/components/SocialSharing";
 import PodcastContextProvider from "@/context/PodcastContext";
-import { Suspense } from "react";
+import Loading from "./loading";
 
 export const revalidate = 10;
 
@@ -44,110 +44,105 @@ export default async function PodcastDetails({
     }
 
     return (
-      <Suspense fallback={<p>Test</p>}>
-        <main className="max-w-[95rem] w-full mx-auto px-4 pb-12 md:pt-8 sm:pt-4 xs:pt-2 md:pb-4 sm:pb-2 xs:pb-2">
-          <PostNavigation href="/podcasts">Podcast</PostNavigation>
-          <div className="max-w-[75rem] w-full mx-auto flex flex-wrap gap-24">
-            <div className="flex flex-col lg:w-1/4">
-              <img
-                className=""
-                src={podcastData.img}
-                alt={podcastData.imgAlt}
+      <Loading />
+      /* <main className="max-w-[95rem] w-full mx-auto px-4 pb-12 md:pt-8 sm:pt-4 xs:pt-2 md:pb-4 sm:pb-2 xs:pb-2">
+        <PostNavigation href="/podcasts">Podcast</PostNavigation>
+        <div className="max-w-[75rem] w-full mx-auto flex flex-wrap gap-24">
+          <div className="flex flex-col lg:w-1/4">
+            <img className="" src={podcastData.img} alt={podcastData.imgAlt} />
+            <div className="flex justify-between mt-8 pb-12 border-b border-black">
+              <p className="text-xl font-semibold">Listen On</p>
+              <SocialSharing
+                links={[
+                  {
+                    href: "#",
+                    ariaLabel: "Visit our Spotify page",
+                    src: "/icons/ri_spotify-fill.svg",
+                    alt: "Instagram logo",
+                  },
+                  {
+                    href: "#",
+                    ariaLabel: "Visit our Apple page",
+                    src: "/icons/ri_apple-fill.svg",
+                    alt: "Twitter logo",
+                  },
+                  {
+                    href: "#",
+                    ariaLabel: "Visit our Soundcloud page",
+                    src: "/icons/ri_soundcloud-line.svg",
+                    alt: "YouTube logo",
+                  },
+                ]}
               />
-              <div className="flex justify-between mt-8 pb-12 border-b border-black">
-                <p className="text-xl font-semibold">Listen On</p>
+            </div>
+            <div className="flex flex-col gap-4 pt-8">
+              <div className="flex flex-wrap justify-between">
+                <p>Date</p>
+                <p>{podcastData.date}</p>
+              </div>
+              <div className="flex flex-wrap justify-between">
+                <p>Duration</p>
+                <p>{podcastData.duration}</p>
+              </div>
+              <div className="flex flex-wrap justify-between">
+                <p className="flex">Share</p>
                 <SocialSharing
                   links={[
                     {
                       href: "#",
-                      ariaLabel: "Visit our Spotify page",
-                      src: "/icons/ri_spotify-fill.svg",
+                      ariaLabel: "Visit our Instagram page",
+                      src: "/icons/ri_instagram-line.svg",
                       alt: "Instagram logo",
                     },
                     {
                       href: "#",
-                      ariaLabel: "Visit our Apple page",
-                      src: "/icons/ri_apple-fill.svg",
+                      ariaLabel: "Visit our Twitter page",
+                      src: "/icons/ri_twitter-fill.svg",
                       alt: "Twitter logo",
                     },
                     {
                       href: "#",
-                      ariaLabel: "Visit our Soundcloud page",
-                      src: "/icons/ri_soundcloud-line.svg",
+                      ariaLabel: "Visit our YouTube page",
+                      src: "/icons/ri_youtube-fill.svg",
                       alt: "YouTube logo",
                     },
                   ]}
                 />
               </div>
-              <div className="flex flex-col gap-4 pt-8">
-                <div className="flex flex-wrap justify-between">
-                  <p>Date</p>
-                  <p>{podcastData.date}</p>
-                </div>
-                <div className="flex flex-wrap justify-between">
-                  <p>Duration</p>
-                  <p>{podcastData.duration}</p>
-                </div>
-                <div className="flex flex-wrap justify-between">
-                  <p className="flex">Share</p>
-                  <SocialSharing
-                    links={[
-                      {
-                        href: "#",
-                        ariaLabel: "Visit our Instagram page",
-                        src: "/icons/ri_instagram-line.svg",
-                        alt: "Instagram logo",
-                      },
-                      {
-                        href: "#",
-                        ariaLabel: "Visit our Twitter page",
-                        src: "/icons/ri_twitter-fill.svg",
-                        alt: "Twitter logo",
-                      },
-                      {
-                        href: "#",
-                        ariaLabel: "Visit our YouTube page",
-                        src: "/icons/ri_youtube-fill.svg",
-                        alt: "YouTube logo",
-                      },
-                    ]}
-                  />
-                </div>
-              </div>
             </div>
+          </div>
 
-            <div className="flex flex-col flex-1 w-full">
-              <p className="uppercase font-semibold">{podcastData.episode}</p>
-              <h1 className="podcast-title">{podcastData.title}</h1>
-              {podcastData.content.map((podcastItem, index) => (
-                <div key={index}>
-                  <p className="text-blog-summary pt-8 pb-16">
-                    {podcastItem.summary}
-                  </p>
-                  <p>{podcastItem.section1}</p>
-                  <div className="border-t-2 border-b-2 border-black my-6">
-                    <div className="py-12">
-                      <p className="text-blog-quote pb-6">
-                        &ldquo;{podcastItem.quote[0]}
-                      </p>
-                      <p> {podcastItem.quote[1]}</p>
-                    </div>
+          <div className="flex flex-col flex-1 w-full">
+            <p className="uppercase font-semibold">{podcastData.episode}</p>
+            <h1 className="podcast-title">{podcastData.title}</h1>
+            {podcastData.content.map((podcastItem, index) => (
+              <div key={index}>
+                <p className="text-blog-summary pt-8 pb-16">
+                  {podcastItem.summary}
+                </p>
+                <p>{podcastItem.section1}</p>
+                <div className="border-t-2 border-b-2 border-black my-6">
+                  <div className="py-12">
+                    <p className="text-blog-quote pb-6">
+                      &ldquo;{podcastItem.quote[0]}
+                    </p>
+                    <p> {podcastItem.quote[1]}</p>
                   </div>
-                  <p>{podcastItem.section2}</p>
                 </div>
-              ))}
-            </div>
+                <p>{podcastItem.section2}</p>
+              </div>
+            ))}
           </div>
-          <div className="pb-12 md:pb-48">
-            <h2 className="text-blog-subheading border-t-2 border-black mt-[9.5rem] pt-12 pb-12 md:pb-24">
-              Latest Episodes
-            </h2>
-            <PodcastContextProvider limit={3}>
-              <LatestPodcasts />
-            </PodcastContextProvider>
-          </div>
-        </main>
-      </Suspense>
+        </div>
+        <div className="pb-12 md:pb-48">
+          <h2 className="text-blog-subheading border-t-2 border-black mt-[9.5rem] pt-12 pb-12 md:pb-24">
+            Latest Episodes
+          </h2>
+          <PodcastContextProvider limit={3}>
+            <LatestPodcasts />
+          </PodcastContextProvider>
+        </div>
+      </main> */
     );
   } catch (error) {
     console.error("Error in PodcastDetails component:", error);
