a9dc8ed4ed82a568841ecb923f134436bc4bb45b
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-11-09 01:11:20 +0000

    removed PodcastContextProvider - refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import LatestPodcastsLoading from "@/components/LatestPodcasts/loading";
 import NewsTicker from "@/components/NewsTicker/NewsTicker";
 import PageTitle from "@/components/PageTitle";
 import Subheading from "@/components/Subheading";
-import PodcastContextProvider from "@/context/PodcastContext";
 import { Suspense } from "react";
 
 export const metadata = {
@@ -16,39 +15,37 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <PodcastContextProvider limit={3}>
-      <main className="flex flex-col min-h-screen max-w-[95rem] w-full mx-auto px-4 lg:pt-0 sm:pt-4 xs:pt-2 lg:pb-4 md:pb-4 sm:pb-2 xs:pb-2">
-        <PageTitle
-          className="sr-only"
-          imgSrc="/images/titles/Art&Life.svg"
-          imgAlt="The words 'Art & Life' in bold uppercase lettering"
-        >
-          Art & Life
-        </PageTitle>
+    <main className="flex flex-col min-h-screen max-w-[95rem] w-full mx-auto px-4 lg:pt-0 sm:pt-4 xs:pt-2 lg:pb-4 md:pb-4 sm:pb-2 xs:pb-2">
+      <PageTitle
+        className="sr-only"
+        imgSrc="/images/titles/Art&Life.svg"
+        imgAlt="The words 'Art & Life' in bold uppercase lettering"
+      >
+        Art & Life
+      </PageTitle>
 
-        <Suspense fallback={<NewsLoading />}>
-          <NewsTicker />
-        </Suspense>
-        <LatestArticles />
-        <Subheading
-          className="text-subheading"
-          url="/podcasts"
-          linkText="All episodes"
-        >
-          Podcast
-        </Subheading>
-        <Suspense fallback={<LatestPodcastsLoading />}>
-          <LatestPodcasts />
-        </Suspense>
-        <Subheading
-          className="text-subheading"
-          url="/authors"
-          linkText="All authors"
-        >
-          Authors
-        </Subheading>
-        <Authors />
-      </main>
-    </PodcastContextProvider>
+      <Suspense fallback={<NewsLoading />}>
+        <NewsTicker />
+      </Suspense>
+      <LatestArticles />
+      <Subheading
+        className="text-subheading"
+        url="/podcasts"
+        linkText="All episodes"
+      >
+        Podcast
+      </Subheading>
+      <Suspense fallback={<LatestPodcastsLoading />}>
+        <LatestPodcasts />
+      </Suspense>
+      <Subheading
+        className="text-subheading"
+        url="/authors"
+        linkText="All authors"
+      >
+        Authors
+      </Subheading>
+      <Authors />
+    </main>
   );
 }
