713cdd6bf495d096ac82a556e086d03a3f61b221
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-11-14 23:27:01 +0000

    populated footer scroll content

--- components/NewsletterTicker.tsx
+++ components/NewsletterTicker.tsx
@@ -1,7 +1,25 @@
 export default function NewsletterTicker() {
   return (
-    <div className="flex flex-wrap bg-black text-white py-5">
-      <span className="pr-6">
+    <div className="flex overflow-hidden bg-black text-white py-5">
+      <span className="flex gap-2 pr-6">
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
+        <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
         <p className="font-semibold text-2xl uppercase">Newsletter+++</p>
       </span>
     </div>
