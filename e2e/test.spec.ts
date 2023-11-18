6f632cd503da16aed3c8ccbddcf348afa72236b0
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-11-18 23:08:18 +0000

    Run second test - PASS

--- e2e/test.spec.ts
+++ e2e/test.spec.ts
@@ -3,3 +3,9 @@ import { test, expect } from "@playwright/test";
 test("should navigate to home page on first render", async ({ page }) => {
   await page.goto("/");
 });
+
+test("should navigate from home page to magazine page", async ({ page }) => {
+  await page.goto("/");
+  await page.getByRole("link", { name: "Magazine" }).click();
+  await expect(page).toHaveURL("/magazine");
+});
