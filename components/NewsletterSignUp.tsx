70f776767cef1eef3c28d7615578f3485260eecc
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-11-19 15:41:29 +0000

    Finish traditional form validation

--- components/NewsletterSignUp.tsx
+++ components/NewsletterSignUp.tsx
@@ -7,27 +7,44 @@ import { Input } from "./ui/input";
 export default function NewsletterSignUp() {
   const [emailAddress, setEmailAddress] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [errors, setErrors] = useState<string[]>([]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     e.preventDefault();
     setIsSubmitting(true);
 
+    if (!regex.test(emailAddress)) {
+      setErrors(["Email address must be the correct format"]);
+      setIsSubmitting(false);
+      return;
+    }
+
     await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    setEmailAddress("");
 
     setIsSubmitting(false);
   };
-
-  console.log(emailAddress);
 
   return (
     <div className="bg-[#f8f8f8] p-[1.88rem] mt-16">
       <h3 className="uppercase font-semibold mb-2">Newsletter</h3>
       <p className="heading3-title mb-4">Design News to your Inbox</p>
       <form onSubmit={handleSubmit}>
+        {errors.length > 0 && (
+          <ul>
+            {errors.map((error) => (
+              <li key={error} className="text-red-500">
+                {error}
+              </li>
+            ))}
+          </ul>
+        )}
         <Input
           className="mb-2"
-          type="email"
-          placeholder="email"
+          type="text"
+          placeholder="Email Address"
           name="email address"
           value={emailAddress}
           onChange={(e) => setEmailAddress(e.currentTarget.value)}
