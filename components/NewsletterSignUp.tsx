63a487f5a46bcff571fe52a24c709b780ccb8b45
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-11-19 15:26:28 +0000

    Start traditional form validation for newsletter signup

--- components/NewsletterSignUp.tsx
+++ components/NewsletterSignUp.tsx
@@ -1,15 +1,46 @@
+"use client";
+
+import { useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 export default function NewsletterSignUp() {
+  const [emailAddress, setEmailAddress] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    setIsSubmitting(false);
+  };
+
+  console.log(emailAddress);
+
   return (
     <div className="bg-[#f8f8f8] p-[1.88rem] mt-16">
       <h3 className="uppercase font-semibold mb-2">Newsletter</h3>
       <p className="heading3-title mb-4">Design News to your Inbox</p>
-      <div>
-        <Input className="mb-2" placeholder="Email" />
-        <Button>Sign Up</Button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <Input
+          className="mb-2"
+          type="email"
+          placeholder="email"
+          name="email address"
+          value={emailAddress}
+          onChange={(e) => setEmailAddress(e.currentTarget.value)}
+          required
+        />
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="disabled:cursor-none"
+        >
+          Sign Up
+        </Button>
+      </form>
     </div>
   );
 }
