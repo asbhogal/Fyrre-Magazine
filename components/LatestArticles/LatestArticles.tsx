a606cb03f1333e650cb4e01a17fa7f628068f71e
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-11-21 12:52:10 +0000

    Add further skeleton details to home page

--- components/LatestArticles/LatestArticles.tsx
+++ components/LatestArticles/LatestArticles.tsx
@@ -9,7 +9,9 @@ import Loading from "./loading";
 export default function LatestArticles() {
   const { data } = useArticleContext();
 
-  if (data.length > 0 && data[0].articles.length > 0) {
+  return <Loading />;
+
+  /* if (data.length > 0 && data[0].articles.length > 0) {
     const allArticles = data[0].articles.sort((a, b) => {
       const dateA = new Date(a.date).getTime();
       const dateB = new Date(b.date).getTime();
@@ -122,5 +124,5 @@ export default function LatestArticles() {
     );
   } else {
     return <Loading />;
-  }
+  } */
 }
--- components/LatestArticles/loading.tsx
+++ components/LatestArticles/loading.tsx
@@ -7,20 +7,31 @@ export default function Loading() {
       <div className="flex flex-col-reverse sm:flex-col gap-12 py-10 max-w-[95rem] w-full mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12">
           <Skeleton className="bg-[#a1a1a1] rounded-none box h-[10.625rem]"></Skeleton>
-          <div className="flex flex-col gap-0.5">
-            <Skeleton className="bg-[#a1a1a1] w-full h-6 rounded-none"></Skeleton>
-            <Skeleton className="bg-[#a1a1a1] w-full h-6 rounded-none"></Skeleton>
-            <Skeleton className="bg-[#a1a1a1] w-full h-6 rounded-none"></Skeleton>
-            <Skeleton className="bg-[#a1a1a1] w-full h-6 rounded-none"></Skeleton>
-            <Skeleton className="bg-[#a1a1a1] w-full h-6 rounded-none"></Skeleton>
-            <div className="flex justify-between sm:items-center gap-2">
-              <div className="flex gap-2 sm:gap-6">
-                <Skeleton className="bg-[#a1a1a1] w-full h-6 rounded-none"></Skeleton>
-                <Skeleton className="bg-[#a1a1a1] w-full h-6 rounded-none"></Skeleton>
-                <Skeleton className="bg-[#a1a1a1] w-full h-6 rounded-none"></Skeleton>
+          <div className="flex flex-col gap-0.5 justify-between">
+            <Skeleton className="bg-[#a1a1a1] w-full h-5 rounded-none"></Skeleton>
+            <Skeleton className="bg-[#a1a1a1] w-full h-5 rounded-none"></Skeleton>
+            <Skeleton className="bg-[#a1a1a1] w-full h-5 rounded-none"></Skeleton>
+            <Skeleton className="bg-[#a1a1a1] w-full h-5 rounded-none"></Skeleton>
+            <Skeleton className="bg-[#a1a1a1] w-full h-5 rounded-none"></Skeleton>
+            <div className="flex justify-between sm:items-center gap-2 mt-2">
+              <div className="flex flex-col lg:flex-row gap-2">
+                <div className="flex gap-2">
+                  <Skeleton className="bg-[#a1a1a1] h-6 w-10 rounded-none" />
+                  <Skeleton className="bg-[#a1a1a1] h-6 w-24 rounded-none" />
+                </div>
+                <div className="flex gap-2">
+                  <Skeleton className="bg-[#a1a1a1] h-6 w-10 rounded-none" />
+                  <Skeleton className="bg-[#a1a1a1] h-6 w-24 rounded-none" />
+                </div>
+                <div className="flex gap-2">
+                  <Skeleton className="bg-[#a1a1a1] h-6 w-10 rounded-none" />
+                  <Skeleton className="bg-[#a1a1a1] h-6 w-24 rounded-none" />
+                </div>
               </div>
-              <div className="bg-[#a1a1a1] rounded-full w-fit">
-                <Skeleton></Skeleton>
+              <div className="flex justify-end">
+                <Skeleton className="bg-white h-[2.625rem] w-[3.5rem] border border-[#a1a1a1] rounded-full flex items-center justify-center">
+                  <Skeleton className="bg-[#a1a1a1] h-6 w-8 rounded-none" />
+                </Skeleton>
               </div>
             </div>
           </div>
