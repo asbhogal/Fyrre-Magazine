89f2e6b5f993705830d831a0b75f477b2ab8cb3e
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-11-21 11:36:42 +0000

    Add UI for loading state

--- app/authors/loading.tsx
+++ app/authors/loading.tsx
@@ -1,3 +1,133 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function Loading() {
-  return <p>TEST</p>;
+  return (
+    <div className="flex flex-col max-w-[95rem] w-full mx-auto py-8 lg:pt-24 lg:pb-48">
+      <div className="flex flex-col md:flex-row justify-between md:items-center gap-2 md:gap-0">
+        <div className="grid grid-cols-1 md:grid-cols-2 md:items-center gap-2 md:gap-16">
+          <Skeleton className="bg-[#a1a1a1] h-[9.375rem] w-[9.375rem] rounded-full" />
+          <Skeleton className="bg-[#a1a1a1] h-4 w-full rounded-none" />
+        </div>
+        <div className="flex flex-col lg:flex-row gap-2 lg:gap-24">
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+          </div>
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+          </div>
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-16 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-4 rounded-none" />
+          </div>
+        </div>
+      </div>
+      <Skeleton className="bg-[#a1a1a1] h-1 w-full my-6" />
+      <div className="flex flex-col md:flex-row justify-between md:items-center gap-2 md:gap-0">
+        <div className="grid grid-cols-1 md:grid-cols-2 md:items-center gap-2 md:gap-16">
+          <Skeleton className="bg-[#a1a1a1] h-[9.375rem] w-[9.375rem] rounded-full" />
+          <Skeleton className="bg-[#a1a1a1] h-4 w-full rounded-none" />
+        </div>
+        <div className="flex flex-col lg:flex-row gap-2 lg:gap-24">
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+          </div>
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+          </div>
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-16 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-4 rounded-none" />
+          </div>
+        </div>
+      </div>
+      <Skeleton className="bg-[#a1a1a1] h-1 w-full my-6" />
+      <div className="flex flex-col md:flex-row justify-between md:items-center gap-2 md:gap-0">
+        <div className="grid grid-cols-1 md:grid-cols-2 md:items-center gap-2 md:gap-16">
+          <Skeleton className="bg-[#a1a1a1] h-[9.375rem] w-[9.375rem] rounded-full" />
+          <Skeleton className="bg-[#a1a1a1] h-4 w-full rounded-none" />
+        </div>
+        <div className="flex flex-col lg:flex-row gap-2 lg:gap-24">
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+          </div>
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+          </div>
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-16 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-4 rounded-none" />
+          </div>
+        </div>
+      </div>
+      <Skeleton className="bg-[#a1a1a1] h-1 w-full my-6" />
+      <div className="flex flex-col md:flex-row justify-between md:items-center gap-2 md:gap-0">
+        <div className="grid grid-cols-1 md:grid-cols-2 md:items-center gap-2 md:gap-16">
+          <Skeleton className="bg-[#a1a1a1] h-[9.375rem] w-[9.375rem] rounded-full" />
+          <Skeleton className="bg-[#a1a1a1] h-4 w-full rounded-none" />
+        </div>
+        <div className="flex flex-col lg:flex-row gap-2 lg:gap-24">
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+          </div>
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+          </div>
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-16 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-4 rounded-none" />
+          </div>
+        </div>
+      </div>
+      <Skeleton className="bg-[#a1a1a1] h-1 w-full my-6" />
+      <div className="flex flex-col md:flex-row justify-between md:items-center gap-2 md:gap-0">
+        <div className="grid grid-cols-1 md:grid-cols-2 md:items-center gap-2 md:gap-16">
+          <Skeleton className="bg-[#a1a1a1] h-[9.375rem] w-[9.375rem] rounded-full" />
+          <Skeleton className="bg-[#a1a1a1] h-4 w-full rounded-none" />
+        </div>
+        <div className="flex flex-col lg:flex-row gap-2 lg:gap-24">
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+          </div>
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+          </div>
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-16 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-4 rounded-none" />
+          </div>
+        </div>
+      </div>
+      <Skeleton className="bg-[#a1a1a1] h-1 w-full my-6" />
+      <div className="flex flex-col md:flex-row justify-between md:items-center gap-2 md:gap-0">
+        <div className="grid grid-cols-1 md:grid-cols-2 md:items-center gap-2 md:gap-16">
+          <Skeleton className="bg-[#a1a1a1] h-[9.375rem] w-[9.375rem] rounded-full" />
+          <Skeleton className="bg-[#a1a1a1] h-4 w-full rounded-none" />
+        </div>
+        <div className="flex flex-col lg:flex-row gap-2 lg:gap-24">
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+          </div>
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-12 rounded-none" />
+          </div>
+          <div className="flex gap-2">
+            <Skeleton className="bg-[#a1a1a1] h-4 w-16 rounded-none" />
+            <Skeleton className="bg-[#a1a1a1] h-4 w-4 rounded-none" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
