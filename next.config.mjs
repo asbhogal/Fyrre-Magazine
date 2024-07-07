37d559e474f08cdf5135e4ad0cab435e643e7657
Aman Bhogal <asbhogal1@live.co.uk>
2024-07-07 11:35:15 +0000

    Test commit without signing

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+import { withPayload } from '@payloadcms/next/withPayload'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    reactCompiler: false
+  }
+}
+
+export default withPayload(nextConfig)
---
+++ payload.config.ts
@@ -0,0 +1,96 @@
+import path from 'path'
+import { en } from 'payload/i18n/en'
+import {
+  lexicalEditor,
+} from '@payloadcms/richtext-lexical'
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { buildConfig } from 'payload'
+import sharp from 'sharp'
+import { fileURLToPath } from 'url'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+if (!process.env.ADMIN_EMAIL || !process.env.ADMIN_PASSWORD) {
+  throw new Error('Both ADMIN_EMAIL and ADMIN_PASSWORD must be set.');
+}
+
+export default buildConfig({
+  editor: lexicalEditor(),
+  collections: [
+    {
+      slug: 'users',
+      auth: true,
+      access: {
+        delete: () => false,
+        update: () => false,
+      },
+      fields: [],
+    },
+    {
+      slug: 'pages',
+      admin: {
+        useAsTitle: 'title',
+      },
+      fields: [
+        {
+          name: 'title',
+          type: 'text',
+        },
+        {
+          name: 'content',
+          type: 'richText',
+        },
+      ],
+    },
+    {
+      slug: 'media',
+      upload: true,
+      fields: [
+        {
+          name: 'text',
+          type: 'text',
+        },
+      ],
+    },
+  ],
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.POSTGRES_URI
+    }
+  }),
+
+  i18n: {
+    supportedLanguages: { en },
+  },
+
+  admin: {
+    autoLogin: {
+      email: process.env.ADMIN_EMAIL,
+      password: process.env.ADMIN_PASSWORD,
+      prefillOnly: true,
+    },
+  },
+  async onInit(payload) {
+    const existingUsers = await payload.find({
+      collection: 'users',
+      limit: 1,
+    })
+
+    if (existingUsers.docs.length === 0) {
+      await payload.create({
+        collection: 'users',
+        data: {
+          email: 'dev@payloadcms.com',
+          password: 'test',
+        },
+      })
+    }
+  },
+  sharp,
+})
